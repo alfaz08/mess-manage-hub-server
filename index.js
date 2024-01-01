ea08c74877baa7193b3032876d6de87aaf9423be
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-01 11:26:42 +0600

    get admin and user by roll after login showdashboard and navbar

--- index.js
+++ index.js
@@ -47,6 +47,20 @@ async function run() {
    })
 
 
+//check admin
+app.get('/users/admin/:email',async(req,res)=>{
+  const email =req.params.email
+  // if(email!==req.decoded.email){
+  //   return res.status(403).send({message:'unauthorized access'})
+  // }
+  const query={email:email}
+  const user =await userCollection.findOne(query)
+  let admin=false
+  if(user){
+    admin=user?.roll==='admin'
+  }
+  res.send({admin})
+})
 
 
 
